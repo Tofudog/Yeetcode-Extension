2f83fd820e05a467c0637885606349141f876484
Tofudog <leodefarias25@gmail.com>
2026-07-08 14:03:08 -0400

    Add top portion of black-box

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="black-box">
+        <div className="top-of-box">
+          <p id="filename">
+            <u>login.json</u>
+          </p>
+          <img id="logo" src="/favicon.svg" alt="Yeetcode logo" />
+        </div>
+        <div className="json-form"></div>
+        <div className="button-navbottom"></div>
+      </div>
+    </>
+  );
 }
 
 export default App;
